a7be5d319b0f6ad21ae37ec092e9793c9c5da5f6
niresh <nireshine@gmail.com>
2025-01-12 19:42:38 +0530

    adding authRoutes

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,20 @@ import express from "express";
 import authRoutes from "./routes/authRoutes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // order is important
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
-// console.log(PORT)
 app.listen(PORT, () => {
   console.log("Serverrrrrrrrr in", PORT);
   connectMongoDB();
