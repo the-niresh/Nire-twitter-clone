5018d8676dccc52ab64d32e81d60103224a87b8e
niresh <nireshine@gmail.com>
2025-01-13 13:08:41 +0530

    added userDetails

---
+++ backend/models/notofication.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const notificationchema = mongoose.Schema(
+  {
+    from: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    to: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ["follow", "like"],
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Notification = mongoose.model("Notification", notificationchema);
+
+export default Notification; 
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import express from "express";
-import authRoutes from "./routes/authRoutes.js";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js"
 import connectMongoDB from "./db/connectMongoDB.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -15,6 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes)
 
 app.listen(PORT, () => {
   console.log("Serverrrrrrrrr in", PORT);
